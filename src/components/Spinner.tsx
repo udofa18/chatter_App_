6c0bb6e89d907349c4b00f2a3cadd0fca1ecfc4a
Ufofa18 <danieludofa452@gmail.com>
2023-06-30 23:26:31 +0100

    ..

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,16 +1,10 @@
-import { DoubleOrbit } 
-from 'react-spinner-animated';
-import 'react-spinner-animated/dist/index.css'
+
 import ClipLoader from "react-spinners/ClipLoader"
 
 const Spinner : React.FC = () => {
   return (
     <div className="w-screen border h-screen p-40 m-auto"> 
-    {/* <p className='text-4xl text-center text-white font-bold opacity-10'>CHATTER APP</p>
-<DoubleOrbit text={"Loading..."} bgColor={"blue"} 
-    center={true} width={"100px"} height={"100px"}/>
-    <span className="loading loading-ring loading-lg text-secondary relative"/>
-     */}
+
      <div className=' center  flex mt-40 '>
       <ClipLoader
         color={"skyblue"}
